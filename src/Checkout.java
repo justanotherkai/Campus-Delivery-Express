95cd6750824259a4d8f369b18fbb5ec91599d8c2
Khairil Hisham <justanotherkai000@gmail.com>
2023-01-19 21:49:37 +0800

    update codes

--- src/Checkout.java
+++ src/Checkout.java
@@ -8,16 +8,23 @@
  * @author AHMAD
  */
 import utils.*;
+import java.util.*;
+import java.io.*;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
 
 public class Checkout{
+    final static int MAX_RECEIPT_LENGTH = 13;
 
     public static void foodCheckout(String[][] FoodItemList){
         Display.clearConsole();
         Display.appHeader();
         Food.displayCart();
         checkFoodStock(Food.userOrderQuantity, FoodItemList);
-        calcAndDispPrice(Food.userOrderQuantity, FoodItemList, Food.serviceFee[Food.locationChose]);
-        updateStock(FoodItemList);
+        String[] receiptDetails = calcAndDispPrice(Food.userOrderQuantity, FoodItemList, Food.serviceFee[Food.locationChose]);
+        printReceipt(receiptDetails, Food.FoodLocation[Food.locationChose]);
+        updateStock(Food.userOrderQuantity, FoodItemList);
     }
 
     // public static void servicesCheckout(String[][] servicesList){
@@ -37,7 +44,8 @@ public class Checkout{
     //this function checks if the stock of user's orders are available
     //prompt the user to update their cart and return false, if stock unavailable
     public static void checkFoodStock(int[] userOrderQuantity,String[][] itemList){
-        while(true){
+        boolean stockAvailable = true;
+        do{
             if(Input.charInputPrompt("Would you like to edit your order? (y/n)") == 'y'){
                 Food.orderFood();
                 Display.clearConsole();
@@ -46,30 +54,73 @@ public class Checkout{
                 continue;
             }
             //checks if stock is available, tell user then continue if there's issue
-            //System.out.println("Stock is unavailable, please edit your order");
-            //continue;
-            break;
-        }
-
+            stockAvailable = true;
+            for(int i = 0; i < itemList.length; i++){
+                if(userOrderQuantity[i] > Integer.parseInt(itemList[i][2])){
+                    System.out.println("Sorry, " + itemList[i][0] + " is out of stock. Please update your order");
+                    stockAvailable = false;
+                }
+            }
+        }while(!stockAvailable);
     }
 
     //this function calculates the food price, service fee, and total price 
-    //while also showing a preview of the receipt then prints it
-    public static void calcAndDispPrice(int[] userOrderQuantity, String[][] itemList, double serviceFee){
-        String[] receiptDetails = new String[10];
-        System.out.println("Calculating and showing receipt preview");
+    //while also showing a preview of the receipt and return the details
+    public static String[] calcAndDispPrice(int[] userOrderQuantity, String[][] itemList, double serviceFee){
+        String[] receiptDetails = new String[MAX_RECEIPT_LENGTH];
+        double totalCost = 0;
+        int receiptLine = 0;
+        Display.clearConsole();
+        Display.appHeader();
+        for(int i = 0; i < itemList.length; i++){
+            if(userOrderQuantity[i] != 0){
+                double price = userOrderQuantity[i]*Double.parseDouble(itemList[i][1]);
+                totalCost += price;
+                String calculations = "- " + itemList[i][0] + " x " + userOrderQuantity[i] + " : RM" + String.format("%.2f", price);
+                System.out.println(calculations);
+                receiptDetails[receiptLine] = calculations;
+                receiptLine++;
+            }
+        }
+        totalCost += serviceFee;
+        String serviceFeeAmt = "Service fee : RM" + String.format("%.2f", serviceFee);
+        System.out.println(serviceFeeAmt);
+        receiptDetails[MAX_RECEIPT_LENGTH - 2] = serviceFeeAmt;
 
-        printReceipt(receiptDetails);// pass lines of receipt details which was printed already
+        String totalCostAmt = "Total : RM" + String.format("%.2f", totalCost);
+        System.out.println(totalCostAmt);
+        receiptDetails[MAX_RECEIPT_LENGTH - 1] = totalCostAmt;
+        return receiptDetails;
     }
 
     //print receipt in a txt file
-    public static void printReceipt(String[] receiptDetails){
-        System.out.println("Receipt was printed");
+    public static void printReceipt(String[] receiptDetails, String location){
+        String straightLine = "\n---------------------------------------------\n";
+        //create a string for date and time with a certain format using LocalDateTIme and DateTimeFormatter objects
+        LocalDateTime dateObj = LocalDateTime.now();
+        String dateAndTimeNow = dateObj.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        String dateAndTimeNowReceipt = dateObj.format(DateTimeFormatter.ofPattern("_dd-MM-yy_HH-mm-ss"));
+        try{
+            File receiptFile = new File("./receipts/Receipt" + dateAndTimeNowReceipt + ".txt");
+            PrintWriter receiptWriter = new PrintWriter(receiptFile);
+            receiptWriter.write(straightLine + "Campus Delivery Express (IIUM GOMBAK)\nLocation : " + location + straightLine);
+            for(int i = 0; i < MAX_RECEIPT_LENGTH; i++){
+                receiptWriter.write((receiptDetails[i] == null ? " " : receiptDetails[i]) + "\n");
+                receiptWriter.flush();
+            }
+            receiptWriter.write(straightLine + "Transaction timestamp : " + dateAndTimeNow + straightLine);
+            receiptWriter.close();
+        }catch(Exception ex){
+            Display.exceptionOccuredMessage();
+        }
     }
 
     //update stock in FoodItem
-    public static void updateStock(String[][] itemList){
-        System.out.println("Stock was updated");
-        Input.charInputPrompt("");
+    public static void updateStock(int[] userOrderQuantity, String[][] itemList){
+        System.out.println("Stock of " + itemList[0][0] + " is " + itemList[0][2]);
+        for(int i = 0; i < itemList.length; i++){
+            itemList[i][2] = Integer.toString(Integer.parseInt(itemList[i][2])-userOrderQuantity[i]);
+        }
+        System.out.println("Stock of " + itemList[0][0] + " is " + itemList[0][2]);
     }
 }
